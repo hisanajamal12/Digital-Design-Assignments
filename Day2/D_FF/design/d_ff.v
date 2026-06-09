2db99a46e29bd54c26daf37f8840a2563be8115e
hisanajamal12 <hisanajamal88@gmail.com>
2026-06-09 21:59:30 +0530

    Update d_ff.v

--- Day2/D_FF/design/d_ff.v
+++ Day2/D_FF/design/d_ff.v
@@ -1,1 +1,15 @@
+module d_flipflop(
+    input d, clk, rst,
+    output reg q, qbar
+);
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            q <= 0; qbar <= 1;
+        end
+        else begin
+            q <= d;
+            qbar <= ~d;
+        end
+    end
+endmodule
 
