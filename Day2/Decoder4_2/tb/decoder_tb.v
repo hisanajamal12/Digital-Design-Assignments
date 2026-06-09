118bec60eeb27d4f868db5c72a60e4128f86591d
hisanajamal12 <hisanajamal88@gmail.com>
2026-06-09 22:03:26 +0530

    Update decoder_tb.v

--- Day2/Decoder4_2/tb/decoder_tb.v
+++ Day2/Decoder4_2/tb/decoder_tb.v
@@ -1,1 +1,23 @@
+module encoder_tb();
+
+    reg [3:0] D;
+    wire [1:0] b;
+    integer m;
+
+   
+    encoder4_2 dut(.D(D), .b(b));
+
+   initial
+    begin
+        D = 0;
+      
+        for(m = 0; m < 4; m = m + 1) 
+        begin 
+            #10;
+            
+        end
+        #10 $finish;
+    end
+
+endmodule
 
