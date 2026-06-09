9ac1f1bf6a4d058cba91ceb4721b8888b232e56c
hisanajamal12 <hisanajamal88@gmail.com>
2026-06-09 22:02:34 +0530

    Update sr_ff_tb.v

--- Day2/SR_FF/tb/sr_ff_tb.v
+++ Day2/SR_FF/tb/sr_ff_tb.v
@@ -1,1 +1,18 @@
+module sr_ff_tb();
+    reg s, r, clk, rst;
+    wire q, qbar;
+    
+    sr_flipflop dut(s, r, clk, rst, q, qbar);
+    
+    // ???????? ??????
+    always #5 clk = ~clk; 
+    
+    initial begin
+        clk = 0; rst = 1; #10;
+        rst = 0;
+        s = 0; r = 1; #10;
+        s = 1; r = 0; #10;
+        $finish;
+    end
+endmodule
 
