094b9b15a1d546b4fca37ab9d54bbf57e8376c92
hisanajamal12 <hisanajamal88@gmail.com>
2026-06-09 22:01:59 +0530

    Update d_ff_tb.v

--- Day2/D_FF/tb/d_ff_tb.v
+++ Day2/D_FF/tb/d_ff_tb.v
@@ -1,1 +1,20 @@
+module d_ff_tb();
+    reg d_tb, clk_tb, rst_tb;
+    wire q_tb, qbar_tb;
 
+    d_flipflop dut(d_tb, clk_tb, rst_tb, q_tb, qbar_tb);
+
+    always #5 clk_tb = ~clk_tb;
+
+    initial begin
+        clk_tb = 0;
+        rst_tb = 1;
+        d_tb = 0;
+        #10 rst_tb = 0;
+        #10 d_tb = 1;
+        #10 d_tb = 0;
+        #10 d_tb = 1;
+        #10 d_tb = 0;
+        #20 $finish;
+    end
+endmodule
